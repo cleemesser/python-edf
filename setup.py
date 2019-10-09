711130594613cb7624543687d4c99fd35adff6ca
Chris Lee-Messer <chris@lee-messer.net>
2019-10-08 22:43:49 -0700

    comment out old way of using build_ext from Cython.Distutils

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+#from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
 import sys
